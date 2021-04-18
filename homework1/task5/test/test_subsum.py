5a7b499bcdd66a9875a4159cc2b9192476dc3c43
JeRusakow <yegorsr0@gmail.com>
2021-04-19 01:13:59 +0300

    Task5 added

---
+++ homework1/task5/test/test_subsum.py
@@ -0,0 +1,8 @@
+from subarray.task05 import find_maximal_subarray_sum
+
+
+def test_subsum():
+    """Test algorithm with simple input"""
+    test_nums = nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    assert find_maximal_subarray_sum(nums, k) == 16
