c940d1283b5e039e358cc5ba0cc79fa265ea3954
JeRusakow <yegorsr0@gmail.com>
2021-04-20 00:19:56 +0300

    Task3 test modified to be run from arbitrary directory

--- tests/test_homework1/test_maximum_and_minimum.py
+++ tests/test_homework1/test_maximum_and_minimum.py
@@ -1,8 +1,12 @@
+import os
+
 from homework1.task3.minmax.task03 import find_maximum_and_minimum
 
 
 def test_max_and_min():
-    filename = "testfile.txt"
-    max_num, min_num = find_maximum_and_minimum(filename)
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    absolute_filename = os.path.join(current_directory, "testfile.txt")
+
+    max_num, min_num = find_maximum_and_minimum(absolute_filename)
     assert max_num == 15
     assert min_num == 2
