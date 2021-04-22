a0599915976227350e94c6b34c92608b6cd4e891
JeRusakow <yegorsr0@gmail.com>
2021-04-22 22:57:03 +0300

    A generator added for getting single symbols from file instead of storing all the file contents in memory

--- homework2/task1/hw1.py
+++ homework2/task1/hw1.py
@@ -7,7 +7,16 @@ Given a file containing text. Complete using only default collections:
     5) Find most common non ascii char for document
 """
 import string
-from typing import List
+from typing import Generator, List
+
+
+def gen_symbols_from_file(filename: str, encoding="utf-8") -> Generator:
+    """Generator for reading a file symbol by symbol"""
+    with open(filename, encoding=encoding) as datafile:
+        data = datafile.read(1)
+        while data:
+            yield data
+            data = datafile.read(1)
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
@@ -31,12 +40,11 @@ def get_rarest_char(file_path: str) -> str:
     """Returns the rarest symbol for a file. If there are several equally rare symbols, returns one of them"""
     chars_dict = {}
 
-    with open(file_path, "r", encoding="unicode-escape") as datafile:
-        for symbol in datafile.read():
-            if symbol not in chars_dict.keys():
-                chars_dict[symbol] = 1
-            else:
-                chars_dict[symbol] += 1
+    for symbol in gen_symbols_from_file(file_path, encoding="unicode-escape"):
+        if symbol not in chars_dict.keys():
+            chars_dict[symbol] = 1
+        else:
+            chars_dict[symbol] += 1
 
     chars_dict = sorted(chars_dict.items(), key=lambda x: x[1])
 
@@ -47,10 +55,9 @@ def count_punctuation_chars(file_path: str) -> int:
     """Returns the amount of punctuations symbols from across the file"""
     punctuation_counter = 0
 
-    with open(file_path, "r", encoding="unicode-escape") as datafile:
-        for symbol in datafile.read():
-            if symbol in string.punctuation:
-                punctuation_counter += 1
+    for symbol in gen_symbols_from_file(file_path, encoding="unicode-escape"):
+        if symbol in string.punctuation:
+            punctuation_counter += 1
 
     return punctuation_counter
 
@@ -59,10 +66,9 @@ def count_non_ascii_chars(file_path: str) -> int:
     """Returns the amount of non-ascii characters from across the file"""
     non_ascii_counter = 0
 
-    with open(file_path, "r", encoding="unicode-escape") as datafile:
-        for symbol in datafile.read():
-            if not symbol.isascii():
-                non_ascii_counter += 1
+    for symbol in gen_symbols_from_file(file_path, encoding="unicode-escape"):
+        if not symbol.isascii():
+            non_ascii_counter += 1
 
     return non_ascii_counter
 
@@ -71,13 +77,12 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
     """Shuffles through the file and returns the most common non-ascii character"""
     non_ascii_char_dict = {}
 
-    with open(file_path, "r", encoding="unicode-escape") as datafile:
-        for symbol in datafile.read():
-            if not symbol.isascii():
-                if symbol not in non_ascii_char_dict.keys():
-                    non_ascii_char_dict[symbol] = 1
-                else:
-                    non_ascii_char_dict[symbol] += 1
+    for symbol in gen_symbols_from_file(file_path, encoding="unicode-escape"):
+        if not symbol.isascii():
+            if symbol not in non_ascii_char_dict.keys():
+                non_ascii_char_dict[symbol] = 1
+            else:
+                non_ascii_char_dict[symbol] += 1
 
     non_ascii_char_dict = sorted(non_ascii_char_dict.items(), key=lambda x: x[1])
 
