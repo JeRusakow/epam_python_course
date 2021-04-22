cdf9c33a1f75cba8fe1f637f7d98740638f6305f
JeRusakow <yegorsr0@gmail.com>
2021-04-22 23:31:43 +0300

    Repeated code of getting current directory moved out into a fixture

--- tests/test_homework2/test_task1/test_task1.py
+++ tests/test_homework2/test_task1/test_task1.py
@@ -1,20 +1,25 @@
 import os
 
+from pytest import fixture
+
 from homework2.task1.hw1 import *
 
 
-def test_get_longest_diverse_words_quantity():
+@fixture()
+def get_curr_directory():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def test_get_longest_diverse_words_quantity(get_curr_directory):
     """Tests if the function returns exactly 10 words"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "longest_words_find_10.txt")
+    absolute_filename = os.path.join(get_curr_directory, "longest_words_find_10.txt")
 
     assert len(get_longest_diverse_words(absolute_filename)) == 10
 
 
-def test_get_longest_diverse_words_correctness():
+def test_get_longest_diverse_words_correctness(get_curr_directory):
     """Tests if the function searches for diverse words but not just longest ones"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "longest_diverse_words.txt")
+    absolute_filename = os.path.join(get_curr_directory, "longest_diverse_words.txt")
 
     correct_result = [
         "abcdefghijk",
@@ -36,35 +41,31 @@ def test_get_longest_diverse_words_correctness():
     assert result == correct_result
 
 
-def test_get_rarest_char():
+def test_get_rarest_char(get_curr_directory):
     """Tests if the function really returns the rarest character"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "rarest_char_e.txt")
+    absolute_filename = os.path.join(get_curr_directory, "rarest_char_e.txt")
 
     correct_result = "e"
 
     assert get_rarest_char(absolute_filename) == correct_result
 
 
-def test_count_punctuation_chars():
+def test_count_punctuation_chars(get_curr_directory):
     """Tests if the function counts punctuations correctly"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "count_punctuation_5.txt")
+    absolute_filename = os.path.join(get_curr_directory, "count_punctuation_5.txt")
 
     assert count_punctuation_chars(absolute_filename) == 5
 
 
-def test_count_non_ascii():
+def test_count_non_ascii(get_curr_directory):
     """Tests if the function counts non-ASCII characters correctly"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "count_non_ascii_12.txt")
+    absolute_filename = os.path.join(get_curr_directory, "count_non_ascii_12.txt")
 
     assert count_non_ascii_chars(absolute_filename) == 12
 
 
-def test_get_most_common_non_ascii():
+def test_get_most_common_non_ascii(get_curr_directory):
     """Tests if the function returns the most common non-ASCII character correctly"""
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    absolute_filename = os.path.join(current_directory, "most_common_non_ascii_ą.txt")
+    absolute_filename = os.path.join(get_curr_directory, "most_common_non_ascii_ą.txt")
 
     assert get_most_common_non_ascii_char(absolute_filename) == "ą"
