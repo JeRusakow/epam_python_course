84fbe0ac8a553f334536da16e8fe27d1f9efeb25
JeRusakow <yegorsr0@gmail.com>
2021-04-25 18:15:24 +0300

    1. Added docstrings for decorator and wrapper-fuction 2. Complex conditional operator divided into two simplier ones in order to make the code clearer

--- homework3/task01/dec_cache.py
+++ homework3/task01/dec_cache.py
@@ -1,12 +1,25 @@
+from collections import defaultdict
 from typing import Callable
 
 
 def dec_cached(times=1) -> Callable:
+    """
+    Caches a function using dictionary. Each function call result is cached for 'times' further calls.
+    After 'times' count expires, original function is called once more and result is saved again for 'times' calls
+    """
+
     def wrapper(func_orig: Callable):
+        """
+        Stores original function output and storing countdown value in a dictionary.
+        """
         result_dict = {}
 
         def cached_func(*args):
-            if args not in result_dict.keys() or result_dict[args][1] == 0:
+            # if function with args was not called previously, call the function
+            if args not in result_dict.keys():
+                result_dict[args] = [func_orig(*args), times]
+            # if storing countdown has expired, call te function again
+            if result_dict[args][1] == 0:
                 result_dict[args] = [func_orig(*args), times]
 
             result_dict[args][1] -= 1
