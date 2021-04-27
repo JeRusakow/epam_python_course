b7990331b91d7ecf3f72f32bed98d15ae65ee152
JeRusakow <yegorsr0@gmail.com>
2021-04-27 19:40:17 +0300

    Multiprocessing task fix

--- homework3/task02/voodoo_calc.py
+++ homework3/task02/voodoo_calc.py
@@ -10,16 +10,43 @@ import time
 # takes approx. 1020 sec and result is 1025932
 def slow_calculate(value):
     """Some weird voodoo magic calculations"""
+    print(f"Calc {value} started")
     time.sleep(random.randint(1, 3))
     data = hashlib.md5(str(value).encode()).digest()
+    print(f"Calc {value} ended")
     return sum(struct.unpack("<" + "B" * len(data), data))
 
 
-def fasterned_voodoo_calc(proc_num=50) -> int:
+def fasterned_voodoo_calc(subroutine_arg_size=10) -> int:
     """
     Uses multiprocessing.Pool to speed up voodoo calculations.
     Proc_num sets the number of non-preemptive processes performing the task.
     """
-    with mp.Pool(proc_num) as pool:
 
-        return sum(pool.map(slow_calculate, [i for i in range(501)]))
+    def worker(shared_value: mp.Value, *args) -> None:
+        for arg in args:
+            res = slow_calculate(arg)
+
+            with shared_value.get_lock():
+                shared_value.value += res
+
+    shared_var = mp.Value("i", 0)
+
+    arg_list = [i for i in range(501)]
+    arg_sublists = [
+        arg_list[i : i + subroutine_arg_size]
+        for i in range(0, len(arg_list), subroutine_arg_size)
+    ]
+
+    processes = [
+        mp.Process(target=worker, args=(shared_var, *arg_sublist))
+        for arg_sublist in arg_sublists
+    ]
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    return shared_var.value
