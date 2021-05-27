d7d6fd973745d68f3549e04cb4327200800ca534
JeRusakow <yegorsr0@gmail.com>
2021-05-27 05:55:47 +0300

    Task2 incapsulation strenghtened

--- homework8/task2/table_data.py
+++ homework8/task2/table_data.py
@@ -12,31 +12,31 @@ class TableData(Collection):
     """
 
     def __init__(self, database_name: str, table_name: str):
-        self._conn = sqlite3.connect(database_name)
-        self._conn.row_factory = sqlite3.Row
-        self._table_name = table_name
+        self.__conn = sqlite3.connect(database_name)
+        self.__conn.row_factory = sqlite3.Row
+        self.__table_name = table_name
 
     def __len__(self) -> int:
-        cursor = self._conn.cursor()
-        cursor.execute(f"select count(*) from {self._table_name}")  # noqa: S608
+        cursor = self.__conn.cursor()
+        cursor.execute(f"select count(*) from {self.__table_name}")  # noqa: S608
         return cursor.fetchone()[0]
 
     def __iter__(self) -> Iterator[sqlite3.Row]:
-        cursor = self._conn.execute(f"select * from {self._table_name}")  # noqa: S608
+        cursor = self.__conn.execute(f"select * from {self.__table_name}")  # noqa: S608
         data = cursor.fetchone()
         while data is not None:
             yield data
             data = cursor.fetchone()
 
     def __contains__(self, __x: object) -> bool:
-        cursor = self._conn.execute(
-            f"select * from {self._table_name} where name = ?", (__x,)  # noqa: S608
+        cursor = self.__conn.execute(
+            f"select * from {self.__table_name} where name = ?", (__x,)  # noqa: S608
         )
         data = cursor.fetchone()
         return data is not None
 
     def __getitem__(self, item: str) -> sqlite3.Row:
-        cursor = self._conn.execute(
-            f"select from {self._table_name} where name= ?", (item,)  # noqa: S608
+        cursor = self.__conn.execute(
+            f"select from {self.__table_name} where name= ?", (item,)  # noqa: S608
         )
         return cursor.fetchone()
